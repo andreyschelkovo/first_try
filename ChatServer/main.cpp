1128e5d52214ba845a4ad20111b194b966653400
andreyschelkovo <andrey.schelkovo@gmail.com>
2024-01-15 21:43:46 +0300

    too

--- ChatServer/main.cpp
+++ ChatServer/main.cpp
@@ -5,16 +5,10 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     Server s;//30 создаём объект нашего класса сервер
-    QSqlDatabase db;
-    db = QSqlDatabase::addDatabase("QSQLITE");
 
-    db.setDatabaseName("./testBD.db");
-    if (db.open()){
-        qDebug ("db open");
-    }
-    else{
-        qDebug ("db no open");
-    }
+
+
+
 
     return a.exec();
 }
